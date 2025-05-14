d95101f5be713a4de7ff299a3d592e64462b728c
HermanMoreno09 <96098987+HermanMoreno09@users.noreply.github.com>
2025-05-14 14:35:16 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           }
         />
         <Route
-          path="/prueba"
+          path="/*"
           element={
             <div className="">
               <div className="" />
