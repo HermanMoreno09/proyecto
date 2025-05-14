684995a25667e9ef20f2f1dd5d5cfb9da3a5f08a
HermanMoreno09 <96098987+HermanMoreno09@users.noreply.github.com>
2025-05-13 23:34:06 -0500

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,18 @@
-import { Sequelize } from "sequelize";
+/*import { Sequelize } from "sequelize";
 const db = new Sequelize("crm_servicios", "root", "", {
     host: "localhost",
     dialect: "mysql"
 })
 
-export default db
+export default db*/
+const db = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQL_ROOT_PASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT || 3306,
+    dialect: "mysql",
+    logging: false
+  }
+);
